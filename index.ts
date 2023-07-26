2afd9dac2bb99d86f30848678274e65a479194a0
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-26 09:43:32 -0300

    [feat] import Task controller, remove old simple server

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import TaskController from './controllers/TaskController'
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
+app.use(express.json());
+app.use('/tasks', TaskController)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
