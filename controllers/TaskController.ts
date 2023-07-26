eb57e202fad5e93d5ed19b78aac0a89cb1ca6bd8
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-26 10:31:34 -0300

    [feat] add TaskSchema and error validation

--- controllers/TaskController.ts
+++ controllers/TaskController.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Task from '../models/Task';
+import Joi from 'joi';
 
 const router = express.Router();
 
@@ -8,13 +9,22 @@ let tasks: Task[] = []
 router.get('/', (req, res) => {
     res.json(tasks);
   });
-  
+ 
+const taskSchema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required()
+})  
 
-router.post('/', (req,res) => {
-    const task = new Task(req.body.title, req.body.description)
-    tasks.push(task)
-    res.status(201).json(task)
-})
+router.post('/', (req, res) => {
+    const { error } = taskSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: 'Invalid task data' });
+    } else {
+      const task = new Task(req.body.title, req.body.description);
+      tasks.push(task);
+      res.status(201).json(task);
+    }
+  });
 
 router.put('/:id', (req,res) => {
     const task = tasks.find(t => t.id === req.params.id)
